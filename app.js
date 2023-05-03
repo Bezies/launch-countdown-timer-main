cac12b95c870695eb8cbfced6f06509e0122f6a3
Quentin <118823218+Bezies@users.noreply.github.com>
2023-05-03 15:01:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,3 @@
-// FONCTION QUI MARCHE
-// const days = document.querySelector("#days-count");
-// const hours = document.querySelector("#hours-count")
-// const minutes = document.querySelector("#minutes-count")
-// const secondes= document.querySelector("#seconds-count")
-
-
-//     function dateDiff(date1, date2) {
-    
-//     let diff = {}
-//     let time = date2 - date1;
-
-//     console.log(date1)
-//     console.log(date2)
-    
-//     time = Math.floor(time/1000);
-//     diff.sec = time % 60;
-//     secondes.innerHTML = diff.sec;
-
-
-//     time = Math.floor((time - diff.sec)/60);
-//     diff.min = time % 60;
-//     minutes.innerHTML = diff.min;
-
-
-//     time = Math.floor((time - diff.min) / 60);
-//     diff.hour = time % 24;
-//     hours.innerHTML = diff.hour;
-
-
-//     time = Math.floor((time - diff.hour) / 24);
-//     diff.day = time;
-//     days.innerHTML = diff.day;
-
-    
-//     return diff;
-// }
-
-// let date1 = new Date();
-// let date2 = new Date(2023, 05, 30);
-// diff = dateDiff(date1, date2)
-
-// window.location.reload();
-
-
-// setInterval(dateDiff, 4000);
-
-
 
 // TEST 2 
 
@@ -56,7 +8,7 @@ const secondes= document.querySelector("#seconds-count")
 
 setInterval(() => {
     let date1 = new Date();
-    let date2 = new Date(2023, 04, 05);
+    let date2 = new Date(2023, 08, 05);
     let diff = {}
     let time = date2 - date1;
     
